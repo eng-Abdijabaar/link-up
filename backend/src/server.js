6d30f73171c7ebb9b9e6bc3e824a5e2440ea6afa
Abdi-jabaar <abdijabaarsaeed@gmail.com>
2025-11-09 14:14:31 +0300

    auth implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,19 +2,38 @@ import express from 'express';
 import cors from 'cors';
 import { ENV } from './config/env.js';
 import { connectDB } from './config/db.js';
+import { clerkMiddleware } from "@clerk/express";
+import { serve } from "inngest/express";
+import { inngest, functions } from './config/inggest.js';
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(clerkMiddleware()); // req.auth will be available in the request object
 
-const PORT = ENV.PORT || 5000;
+
+app.use("/api/inngest", serve({ client: inngest, functions}));
 
 app.get('/', (req, res) => {
   res.send('LinkUp Backend is running');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-  connectDB();
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+    if (ENV.NODE_ENV !== "production") {
+      app.listen(ENV.PORT, () => {
+        console.log("Server started on port:", ENV.PORT);
+      });
+    }
+  } catch (error) {
+    console.error("Error starting server:", error);
+    process.exit(1); // Exit the process with a failure code
+  }
+};
+
+startServer();
+
+export default app;
 
